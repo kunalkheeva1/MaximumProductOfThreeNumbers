03924d9fe88028431c9434091e0823876bdf2267
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-17 19:11:47 -0700

    coded a method to return the maximum product of three numbers of an array, condition is that the numbers could be negative as well, so read the comments carefully

--- src/MaximumProductOfThreeNumbers.java
+++ src/MaximumProductOfThreeNumbers.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class MaximumProductOfThreeNumbers {
-    
+    public static int maximumProductOfThreeNumbers(int[] arr){
+        int n = arr.length;
+        Arrays.sort(arr);
+        int choice1 = arr[0]*arr[1]*arr[n-1];
+        int choice2 = arr[n-1]*arr[n-2]*arr[n-3];
+
+        return Math.max(choice1, choice2);
+    }
 
     public static void main(String[] args) {
 
