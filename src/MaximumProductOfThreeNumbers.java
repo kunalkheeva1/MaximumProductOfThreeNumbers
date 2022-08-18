e697e0c67764d0968c88e307f83124db39123e63
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-17 19:14:48 -0700

    added the comments for the better understanding

--- src/MaximumProductOfThreeNumbers.java
+++ src/MaximumProductOfThreeNumbers.java
@@ -1,5 +1,12 @@
 import java.util.*;
 public class MaximumProductOfThreeNumbers {
+
+    //return the max product of three sums,
+    // as the numbers could be negative as well, so get two choices
+    // choice 1 deals with negative numbers as well, if the array is sorted
+    // then multiply two negative numbers first and then the last number to get a positive result
+    // in choice2 multiply the last three numbers of the sorted array
+    // and then return the maximum out of choice1 and choice2
     public static int maximumProductOfThreeNumbers(int[] arr){
         int n = arr.length;
         Arrays.sort(arr);
